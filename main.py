c3f48e2c0f7e8c52a9aa33e9eba3461abcabc40e
Roman <54048747+Good4lien@users.noreply.github.com>
2023-03-10 09:43:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests, lxml
+from bs4 import BeautifulSoup as bs
+
+url="https://www.rbc.ru/economics/"
+
+headers= {
+    "user-agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
+    "X-Requested-Width":"XMLHttpRequest"
+}
+
+resp = requests.get(url,headers=headers)
+
+soup = bs(resp.text, "lxml")
+#cat=soup.class('category')
+
+news= soup.find_all(class_= "item__title")
+for n in news:
+    print(n.text)
