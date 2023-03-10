05c885e2a593f93960c03e3a2989d7cba51d21e5
Roman <54048747+Good4lien@users.noreply.github.com>
2023-03-10 11:57:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import requests, lxml
 from bs4 import BeautifulSoup as bs
+from datetime import date, datetime
 
-url="https://www.rbc.ru/economics/"
+def dt(date_):
+    dt=''
+    if ',' in date_:
+        #dt=d.split(',')
+        dt=date_
+        if 'мар' in date_:
+            dt='2023-03-'+date_.split(' ')[0]+date_.split(',')[1]
+    else:
+        dt=str(date.today())+' '+date_
+    return dt
 
-headers= {
-    "user-agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
-    "X-Requested-Width":"XMLHttpRequest"
-}
+def main():
+    url="https://www.rbc.ru/economics/"
 
-resp = requests.get(url,headers=headers)
+    headers= {
+        "user-agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
+        "X-Requested-Width":"XMLHttpRequest"
+    }
 
-soup = bs(resp.text, "lxml")
-#cat=soup.class('category')
+    resp = requests.get(url,headers=headers)
 
-news= soup.find_all(class_= "item__wrap")
+    soup = bs(resp.text, "lxml")
+    #cat=soup.class('category')
 
-for n in news:
-    data=n.find(class_='item__category').text.strip()
-    name = n.find(class_='item__title').text.strip()
-    print(data,name)
+    news= soup.find_all(class_= "item__wrap")
 
+    for n in news:
+        date_ = dt(n.find(class_='item__category').text.strip())
+        name = n.find(class_='item__title').text.strip()
+        print(date_,name)
 
+    #print(str(datetime.now()).split('.')[0])
 
+if __name__ == "__main__":
+    main()
 
 
 
